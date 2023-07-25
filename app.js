e20f5c4e6a695b6fe1b0eeec7008ac68e920b596
sergio <sergio.martinez.sm@outlook.com>
2023-07-25 20:18:32 -0300

    Agregue start a scripts en package.json

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.listen(3000, () => {
-    console.log("Server corriendo");
-});
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Servidor corriendo");
+})
 
 app.get ('/', (req,res) => {
     res.sendFile(path.join (__dirname, '/views/home.html'))
